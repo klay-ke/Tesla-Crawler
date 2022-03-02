05eb407f6d1d4e664cfb89aa3b5b45a562ed490d
Klay <kelingyun0706@gmail.com>
2022-03-01 20:23:33 -0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # Tesla-Crawler
-# Tesla-Crawler
--- main.js
+++ main.js
@@ -47,6 +47,7 @@ const sendEmail = () => {
 
 request(options, function (error, response) {
   if (error) throw new Error(error);
+  console.log(JSON.parse(response.body)[0]);
   if (JSON.parse(response.body)[0].purchasable === true) {
     sendMail();
   }
